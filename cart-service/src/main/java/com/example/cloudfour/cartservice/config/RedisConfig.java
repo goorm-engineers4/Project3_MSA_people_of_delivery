2f4571293200e7120b56e402a5c3a6a31f66d3b0
shieun <shieunmo@gmail.com>
2025-09-03 16:50:49 +0900

    🔨 refactor: redis config 생성 - 기존 cacheconfig 삭제 - redis config 추가

---
+++ cart-service/src/main/java/com/example/cloudfour/cartservice/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.example.cloudfour.cartservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper())));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .withCacheConfiguration("stores", config.entryTtl(Duration.ofHours(1)))
+                .withCacheConfiguration("menus", config.entryTtl(Duration.ofMinutes(30)))
+                .withCacheConfiguration("menuOptions", config.entryTtl(Duration.ofMinutes(30)))
+                .withCacheConfiguration("users", config.entryTtl(Duration.ofMinutes(15)))
+                .build();
+    }
+}
+
+
