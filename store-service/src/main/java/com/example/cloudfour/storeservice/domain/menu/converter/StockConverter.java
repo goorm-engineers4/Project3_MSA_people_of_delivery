ac0e8b93d4f94c275c59e5ff0b59bce8cd66c265
JUNE <jh010303@naver.com>
2025-09-05 16:56:19 +0900

    :sparkles: feat: 재고 조회에 version 제거

--- store-service/src/main/java/com/example/cloudfour/storeservice/domain/menu/converter/StockConverter.java
+++ store-service/src/main/java/com/example/cloudfour/storeservice/domain/menu/converter/StockConverter.java
@@ -3,13 +3,23 @@ package com.example.cloudfour.storeservice.domain.menu.converter;
 import com.example.cloudfour.storeservice.domain.menu.dto.StockResponseDTO;
 import com.example.cloudfour.storeservice.domain.menu.entity.Stock;
 
+import java.util.UUID;
+
 public class StockConverter {
     public static StockResponseDTO toStockResposneDTO(Stock stock){
         return StockResponseDTO.builder()
                 .stockId(stock.getId())
                 .menuId(stock.getMenu().getId())
                 .quantity(stock.getQuantity())
-                .version(stock.getVersion())
+                .build();
+    }
+
+    public static StockResponseDTO CachetoStockResposneDTO(UUID stockId,
+           UUID menuId, Long quantity){
+        return StockResponseDTO.builder()
+                .stockId(stockId)
+                .menuId(menuId)
+                .quantity(quantity)
                 .build();
     }
 }
