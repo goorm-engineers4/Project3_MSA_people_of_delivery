699153b3e594c797f77f4c79db5c56c1ed26edcc
shieun <shieunmo@gmail.com>
2025-09-11 10:39:29 +0900

    🐛 fix: auth-service 내 feignconfig 추가 - header 동록 intercepter 추가

---
+++ auth-service/src/main/java/com/example/cloudfour/authservice/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.example.cloudfour.authservice.config;
+
+import com.example.cloudfour.modulecommon.passport.interceptor.FeignPassportInterceptor;
+import com.example.cloudfour.modulecommon.util.PassportUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public FeignPassportInterceptor feignPassportInterceptor(PassportUtil passportUtil){
+        return new FeignPassportInterceptor(passportUtil);
+    }
+}
