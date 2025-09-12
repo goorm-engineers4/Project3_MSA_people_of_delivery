f3b780f4bf679e1bbadc13ee314dd81b2bb12a99
JUNE <jh010303@naver.com>
2025-09-12 13:07:05 +0900

    :sparkles: feat: MDC 적용

---
+++ user-service/src/main/java/com/example/cloudfour/userservice/filter/MDCFilter.java
@@ -0,0 +1,31 @@
+package com.example.cloudfour.userservice.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MDCFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String traceId = UUID.randomUUID().toString().replaceAll("-", "");
+        MDC.put("traceId", traceId);
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
