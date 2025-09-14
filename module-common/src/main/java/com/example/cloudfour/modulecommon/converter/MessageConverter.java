0a1d5ab2f3cccb7f78ba716bc89286f3649c593f
shieun <shieunmo@gmail.com>
2025-09-14 11:11:14 +0900

    ✨ feat: OutboxEventReadyToPublish converter 추가

--- module-common/src/main/java/com/example/cloudfour/modulecommon/converter/MessageConverter.java
+++ module-common/src/main/java/com/example/cloudfour/modulecommon/converter/MessageConverter.java
@@ -3,6 +3,7 @@ package com.example.cloudfour.modulecommon.converter;
 import com.example.cloudfour.modulecommon.messaging.Envelope;
 import com.example.cloudfour.modulecommon.messaging.MsgMeta;
 import com.example.cloudfour.modulecommon.outbox.entity.OutboxEvent;
+import com.example.cloudfour.modulecommon.outbox.event.OutboxEventReadyToPublish;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -31,6 +32,25 @@ public class MessageConverter {
     }
 
     public static <T> Envelope<T> toEnvelope(OutboxEvent event, T payload) {
+        MsgMeta meta = toMsgMeta(event);
+        return toEnvelope(meta, payload);
+    }
+
+    public static MsgMeta toMsgMeta(OutboxEventReadyToPublish event) {
+        return MsgMeta.builder()
+                .msgId(UUID.randomUUID().toString())
+                .sagaId(event.getAggregateId())
+                .type(event.getEventType())
+                .version("v1")
+                .source("outbox-publisher")
+                .causationId(null)
+                .correlationId(event.getAggregateId())
+                .timestamp(Instant.now())
+                .attempt(0)
+                .build();
+    }
+
+    public static <T> Envelope<T> toEnvelope(OutboxEventReadyToPublish event, T payload) {
         MsgMeta meta = toMsgMeta(event);
         return toEnvelope(meta, payload);
     }
