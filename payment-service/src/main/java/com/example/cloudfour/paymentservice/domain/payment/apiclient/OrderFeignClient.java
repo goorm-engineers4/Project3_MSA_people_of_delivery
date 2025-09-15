9de7a036f3881b617bc9edeffbe2a0aab29d9461
shieun <shieunmo@gmail.com>
2025-09-15 11:29:39 +0900

    🔨 refactor: build 에러 수정 - 에러 import 제거

--- payment-service/src/main/java/com/example/cloudfour/paymentservice/domain/payment/apiclient/OrderFeignClient.java
+++ payment-service/src/main/java/com/example/cloudfour/paymentservice/domain/payment/apiclient/OrderFeignClient.java
@@ -2,7 +2,6 @@ package com.example.cloudfour.paymentservice.domain.payment.apiclient;
 
 import com.example.cloudfour.paymentservice.commondto.OrderResponseDTO;
 import com.example.cloudfour.paymentservice.config.FeignConfig;
-import com.example.cloudfour.paymentservice.domain.payment.dto.OrderStatusUpdateRequestDTO;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
