667725c6ffa18fa56c181152639fa46b7ff67c1a
shieun <shieunmo@gmail.com>
2025-09-15 12:39:03 +0900

    🚀 chore: eureka 설정 제거 - eureka client, server import 제거 - 미제거 어노테이션 제거

--- module-common/src/main/java/com/example/cloudfour/modulecommon/ModuleCommonApplication.java
+++ module-common/src/main/java/com/example/cloudfour/modulecommon/ModuleCommonApplication.java
@@ -2,7 +2,6 @@ package com.example.cloudfour.modulecommon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ModuleCommonApplication {
