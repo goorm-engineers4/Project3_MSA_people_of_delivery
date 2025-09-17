b2c8ab676789573e69f89035710145ea745080ce
shieun <shieunmo@gmail.com>
2025-09-17 20:13:26 +0900

    🚀 chore: cloud 마이그레이션

--- payment-service/src/main/java/com/example/cloudfour/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/example/cloudfour/paymentservice/PaymentServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.cloudfour.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @EnableFeignClients
-@EnableDiscoveryClient
 public class PaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
