ea4ebd8f74743541f212d77448e6c723a9243cc7
shieun <shieunmo@gmail.com>
2025-09-17 20:12:55 +0900

    🚀 chore: cloud 마이그레이션

--- store-service/src/main/java/com/example/cloudfour/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/com/example/cloudfour/storeservice/StoreServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.cloudfour.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @EnableFeignClients
-@EnableDiscoveryClient
 public class StoreServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreServiceApplication.class, args);
