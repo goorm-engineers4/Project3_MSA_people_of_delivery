8d2e90474d44b270deb25bf92b905ca67e939115
JIYOOnii <74430595+JIYOOnii007@users.noreply.github.com>
2025-09-17 20:11:20 +0900

    cloud migration

--- apigateway/src/main/java/com/example/cloudfour/apigateway/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/example/cloudfour/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,10 @@ package com.example.cloudfour.apigateway;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
